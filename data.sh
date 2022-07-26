f922dafb427b76feab3c290af0ba1b45a8f1127e
kimikkorow <3337017542@qq.com>
2022-07-26 20:55:03 +0800

    add sever imge

--- data.sh
+++ data.sh
@@ -12,39 +12,5 @@ echo "安装依赖"
 apt-get install sudo nano git wget curl openjdk-17-jdk openjdk-17-jre -y
 
 # 当前目录 /app
-mkdir gitdate
 mkdir Grasscutter
-
-cd ./gitdate
-# 当前目录/app/gitdate
-
-echo "拉取Grasscutters/Grasscutter仓库"
-git clone https://github.com/Grasscutters/Grasscutter.git
-
-echo "构建Grasscutter.jar"
-
-cd ./Grasscutter
-# 当前目录/app/gitdate/Grasscutter
-chmod +x gradlew
-./gradlew jar
-
-mv *.jar ../../Grasscutter/grasscutter.jar
-mv keystore.p12 ../../Grasscutter/
-
-cd ../../gitdate
-# 当前目录/app/gitdate
-
-echo "拉取Koko-boya/Grasscutter_Resources仓库"
-git clone https://github.com/Koko-boya/Grasscutter_Resources.git
-
-cd ../Grasscutter 
-# 当前目录/app/Grasscutter
-mkdir resources
-
-mv ../gitdate/Grasscutter_Resources/Resources/* ./resources
-
-echo "创建插件目录"
-mkdir plugins
-
-rm -rf /app/gitdate
 
--- start.sh
+++ start.sh
@@ -1,18 +1,3 @@
-echo "更新插件"
-cd /app/Grasscutter/plugins
-rm -rf *.jar
-curl ${github_proxy}https://github.com/liujiaqi7998/GrasscuttersWebDashboard/releases/download/${GrasscuttersWebDashboard}/GrasscuttersWebDashboard-${GrasscuttersWebDashboard}.jar -O
-curl ${github_proxy}https://github.com/jie65535/gc-opencommand-plugin/releases/download/v${opencommand}/opencommand-dev-${opencommand}.jar -O
-
-cd /app/Grasscutter
-
-echo "生成所需文件"
-java -jar grasscutter.jar
-
-echo "设置config.json"
-sed -i "s#127.0.0.1#${accessAddress}#g" `grep 127.0.0.1 -rl ./config.json`
-sed -i "s#443#${bindPort}#g" `grep 443 -rl ./config.json`
-sed -i "s#localhost:27017#${mongodb}#g" `grep localhost:27017 -rl ./config.json`
-
 echo "运行jar"
+cd /app/Grasscutter
 java -jar grasscutter.jar
